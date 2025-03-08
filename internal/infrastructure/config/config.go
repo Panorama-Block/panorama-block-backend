325cbcfb3e6708856789b56d343d0ccbd9e7adac
noymaxx <hugo.povoleri@sou.inteli.edu.br>
2025-03-08 12:59:22 -0300

    feat: call new config and new methods

--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -2,13 +2,23 @@ package config
 
 import (
     "os"
+    "fmt"
+    "time"
+
+    "github.com/redis/go-redis/v9"
+    "context"
 )
 
 type Config struct {
-    ServerPort string
+    ServerPort  string
     RangoAPIKey string
     MongoURI    string
     MongoDBName string
+
+    // Redis
+    RedisHost     string
+    RedisPort     string
+    RedisPassword string
 }
 
 func LoadConfig() *Config {
@@ -18,9 +28,31 @@ func LoadConfig() *Config {
     }
 
     return &Config{
-        ServerPort:  port,
-        RangoAPIKey: os.Getenv("X_RANGO_ID"),
-        MongoURI:    os.Getenv("MONGO_URI"),
-        MongoDBName: os.Getenv("MONGO_DB_NAME"),
+        ServerPort:    port,
+        RangoAPIKey:   os.Getenv("X_RANGO_ID"),
+        MongoURI:      os.Getenv("MONGO_URI"),
+        MongoDBName:   os.Getenv("MONGO_DB_NAME"),
+        RedisHost:     os.Getenv("REDIS_HOST"),
+        RedisPort:     os.Getenv("REDIS_PORT"),
+        RedisPassword: os.Getenv("REDIS_PASS"),
     }
+}
+
+// Conexão simples com Redis
+func ConnectRedis(conf *Config) (*redis.Client, error) {
+    if conf.RedisHost == "" {
+        return nil, fmt.Errorf("Redis not configured")
+    }
+    client := redis.NewClient(&redis.Options{
+        Addr:        fmt.Sprintf("%s:%s", conf.RedisHost, conf.RedisPort),
+        Password:    conf.RedisPassword,
+        DB:          0,
+        DialTimeout: 5 * time.Second,
+    })
+    // teste de conexão
+    _, err := client.Ping(context.Background()).Result()
+    if err != nil {
+        return nil, err
+    }
+    return client, nil
 }
